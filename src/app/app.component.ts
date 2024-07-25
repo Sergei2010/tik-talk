a10c0fa029ce4e8eefef602a4f0fbd761583ddff
Sergey <sergejmatvejcuk323@gmail.com>
2024-07-25 10:57:11 +0000

    first commit
    Signed-off-by: Sergey <sergejmatvejcuk323@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
 
-@Component({
+@Component( {
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, ProfileCardComponent ],
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+  styleUrl: './app.component.scss',
+} )
 export class AppComponent {
-  title = 'tik-talk';
 }
