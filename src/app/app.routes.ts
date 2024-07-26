89ac93f4be00c3a0b22a09a92e8448a3360f151b
Sergey <sergejmatvejcuk323@gmail.com>
2024-07-26 07:03:38 +0000

    make patch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutComponent } from './common-ui/layout/layout.component';
 import { canActivateAuth } from './auth/access.guard';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 export const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
       { path: '', component: SearchPageComponent },
       { path: 'profile/:id', component: ProfilePageComponent },
+      { path: 'settings', component: SettingsPageComponent },
     ],
     canActivate: [ canActivateAuth ]
   },
